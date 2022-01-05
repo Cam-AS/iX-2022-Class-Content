0b3f66d0e1a74cb547a32d5809d10291b124454d
Cam <62012343+Cam-AS@users.noreply.github.com>
2022-01-05 21:04:26 +0200

    added contact book

---
+++ Day_3/contact-book/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { ContactListComponent } from './components/contact-list/contact-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactFormComponent,
+    ContactListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
