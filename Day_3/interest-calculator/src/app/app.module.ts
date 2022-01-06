ac7571702d52c5c596c161412c4159fd41992ad0
Cam <62012343+Cam-AS@users.noreply.github.com>
2022-01-06 09:24:40 +0200

    added interest-calculator

---
+++ Day_3/interest-calculator/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms';
+import { InterestCalculatorComponent } from './components/interest-calculator/interest-calculator.component';
+import { ParentComponent } from './components/parent/parent.component';
+import { ChildComponent } from './components/child/child.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InterestCalculatorComponent,
+    ParentComponent,
+    ChildComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
