022be33ee92c65ccfa9a245c330a6198a5b49ed6
Ryan Ballantine <rgballantine@gmail.com>
2022-01-07 19:58:55 +0200

    Draft of Movie guesser app

---
+++ Day_4/movie-guesser/src/app/components/movie-form/movie-form.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { movieList, Movie } from 'src/app/models/movies';
+
+@Component({
+  selector: 'app-movie-form',
+  templateUrl: './movie-form.component.html',
+  styleUrls: ['./movie-form.component.css']
+})
+export class MovieFormComponent implements OnInit {
+  movieForm: Movie = new Movie();
+  randomMovie: Movie = new Movie();
+  userGuess: Movie = new Movie();
+  show: boolean = false;
+  answer?: string;
+
+  movies = movieList;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.randomMovie = this.getRandomMovie();
+  }
+
+  getRandomMovie() {
+    return this.movies[Math.floor(Math.random() * movieList.length)];
+  }
+
+  next() {
+    this.show = false;
+    this.answer = '';
+    this.randomMovie = this.getRandomMovie();
+  }
+
+  hint() {
+    this.show = !this.show;
+  }
+
+  checkGuess() {
+    if (this.userGuess.title === this.randomMovie.title) {
+      this.answer = ("Noice");
+    }
+    else {
+      this.answer = ("Sorry Mate, try again");
+    }
+
+    this.userGuess = new Movie();
+  }
+}
