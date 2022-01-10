af1e6339d69056d83cde5b942fb84efb183f6e02
Cam <62012343+Cam-AS@users.noreply.github.com>
2022-01-10 16:58:48 +0200

    updates REST API

--- Day_6/rest-api/app.js
+++ Day_6/rest-api/app.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Authorization');
+  next();
+});
+
 app.use('/feed', feedRoutes);
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000!');
-});
+mongoose
+  .connect(
+    'mongodb+srv://cameron:uQViFz9xAE02qAO3@cluster0.yg2fe.mongodb.net/Cluster0?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
--- Day_6/rest-api/routes/feed.js
+++ Day_6/rest-api/routes/feed.js
@@ -5,5 +5,8 @@ const feedController = require('../controllers/feed');
 const router = express.Router();
 
 router.get('/posts', feedController.getPosts);
+router.post('/post', feedController.createPost);
+router.delete('/:id', feedController.deletePost);
+router.put('/:id', feedController.updatePost);
 
 module.exports = router;
